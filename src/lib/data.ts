0b850713a8d837496a15eb355fcf0fe4d646695e
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-09 17:13:35 +0800

    init

---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+type Data = {
+  title: string;
+  description: string;
+  tweets: string[];
+}[];
+
+const data: Data = [
+  {
+    title: "React",
+    description: "A JavaScript library for building user interfaces",
+    tweets: [
+      "https://twitter.com/lynxluna/status/1765387033026150820",
+      "https://twitter.com/gadingnstn/status/1766353424126476406",
+      "https://twitter.com/ainunnajib/status/1766385680916328700",
+    ],
+  },
+];
