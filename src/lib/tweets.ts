c0c29fae61d0ff75b78520bdb3471d8162039423
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-10 09:33:13 +0800

    feat: add more tweet

--- src/lib/tweets.ts
+++ src/lib/tweets.ts
@@ -13,9 +13,24 @@ export const debates: Debates = [
     description:
       "Dimulai dari pembahasan tentang konfigurasi, lalu berlanjut ke topik yang lebih seru.",
     tweets: [
-      "https://twitter.com/lynxluna/status/1765387033026150820",
-      "https://twitter.com/gadingnstn/status/1766353424126476406",
-      "https://twitter.com/ainunnajib/status/1766385680916328700",
+      "https://twitter.com/lynxluna/status/1765387033026150820", // 09:40 PM · Mar 6, 2024
+      "https://twitter.com/gadingnstn/status/1765730273121931653", // 08:24 PM · Mar 7, 2024
+      "https://twitter.com/glendmaatita/status/1765899052913508505", // 07:34 AM · Mar 8, 2024
+      "https://twitter.com/itsfaqih_/status/1765946838828650749", // 10:45 AM · Mar 8, 2024
+      "https://twitter.com/spacerocc/status/1766125691844870249", // 10:35 PM · Mar 8, 2024
+      "https://twitter.com/justmas2biasa/status/1766032926523957700", // 04:27 PM · Mar 8, 2024
+      "https://twitter.com/lynxluna/status/1766246109574557887", // 06:34 AM · Mar 9, 2024
+      "https://twitter.com/effendii_/status/1766313916936986873", // 11:03 AM · Mar 9, 2024
+      "https://twitter.com/Aoohan/status/1766341607056679223", // 12:53 PM · Mar 9, 2024
+      "https://twitter.com/gadingnstn/status/1766353424126476406", // 01:40 PM · Mar 9, 2024
+      "https://twitter.com/maulanafikri455/status/1766357540206772724", // 01:57 PM · Mar 9, 2024
+      "https://twitter.com/ainunnajib/status/1766385680916328700", // 03:48 PM · Mar 9, 2024
+      "https://twitter.com/lynxluna/status/1766393800069648691", // 04:21 PM · Mar 9, 2024
+      "https://twitter.com/gadingnstn/status/1766399528142283007", // 04:44 PM · Mar 9, 2024
+      "https://twitter.com/itsfaqih_/status/1766402190242123961", // 04:54 PM · Mar 9, 2024
+      "https://twitter.com/glendmaatita/status/1766417693677433131", // 05:56 PM · Mar 9, 2024
+      "https://twitter.com/gadingnstn/status/1766487332528504988", // 10:32 PM · Mar 9, 2024
+      "https://twitter.com/itsfaqih_/status/1766483265224151166", // 10:16 PM · Mar 9, 2024
     ],
   },
   {
