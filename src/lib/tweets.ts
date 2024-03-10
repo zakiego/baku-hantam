a393f556f06bfcdae90345cd98f64a4ff32d6804
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-10 09:48:55 +0800

    feat: add tweet performance

--- src/lib/tweets.ts
+++ src/lib/tweets.ts
@@ -13,24 +13,42 @@ export const debates: Debates = [
     description:
       "Dimulai dari pembahasan tentang konfigurasi, lalu berlanjut ke topik yang lebih seru.",
     tweets: [
-      "https://twitter.com/lynxluna/status/1765387033026150820", // 09:40 PM · Mar 6, 2024
-      "https://twitter.com/gadingnstn/status/1765730273121931653", // 08:24 PM · Mar 7, 2024
-      "https://twitter.com/glendmaatita/status/1765899052913508505", // 07:34 AM · Mar 8, 2024
+      "https://twitter.com/lynxluna/status/1765387033026150820", // 9:40 PM · Mar 6, 2024
+
+      "https://twitter.com/gadingnstn/status/1765730273121931653", // 8:24 PM · Mar 7, 2024
+
+      "https://twitter.com/glendmaatita/status/1765899052913508505", // 7:34 AM · Mar 8, 2024
       "https://twitter.com/itsfaqih_/status/1765946838828650749", // 10:45 AM · Mar 8, 2024
       "https://twitter.com/spacerocc/status/1766125691844870249", // 10:35 PM · Mar 8, 2024
-      "https://twitter.com/justmas2biasa/status/1766032926523957700", // 04:27 PM · Mar 8, 2024
-      "https://twitter.com/lynxluna/status/1766246109574557887", // 06:34 AM · Mar 9, 2024
+      "https://twitter.com/justmas2biasa/status/1766032926523957700", // 4:27 PM · Mar 8, 2024
+
+      "https://twitter.com/lynxluna/status/1766246109574557887", // 6:34 AM · Mar 9, 2024
       "https://twitter.com/effendii_/status/1766313916936986873", // 11:03 AM · Mar 9, 2024
       "https://twitter.com/Aoohan/status/1766341607056679223", // 12:53 PM · Mar 9, 2024
-      "https://twitter.com/gadingnstn/status/1766353424126476406", // 01:40 PM · Mar 9, 2024
-      "https://twitter.com/maulanafikri455/status/1766357540206772724", // 01:57 PM · Mar 9, 2024
-      "https://twitter.com/ainunnajib/status/1766385680916328700", // 03:48 PM · Mar 9, 2024
-      "https://twitter.com/lynxluna/status/1766393800069648691", // 04:21 PM · Mar 9, 2024
-      "https://twitter.com/gadingnstn/status/1766399528142283007", // 04:44 PM · Mar 9, 2024
-      "https://twitter.com/itsfaqih_/status/1766402190242123961", // 04:54 PM · Mar 9, 2024
-      "https://twitter.com/glendmaatita/status/1766417693677433131", // 05:56 PM · Mar 9, 2024
+      "https://twitter.com/gadingnstn/status/1766353424126476406", // 1:40 PM · Mar 9, 2024
+      "https://twitter.com/maulanafikri455/status/1766357540206772724", // 1:57 PM · Mar 9, 2024
+      "https://twitter.com/ainunnajib/status/1766385680916328700", // 3:48 PM · Mar 9, 2024
+      "https://twitter.com/lynxluna/status/1766393800069648691", // 4:21 PM · Mar 9, 2024
+      "https://twitter.com/gadingnstn/status/1766399528142283007", // 4:44 PM · Mar 9, 2024
+      "https://twitter.com/itsfaqih_/status/1766402190242123961", // 4:54 PM · Mar 9, 2024
+      "https://twitter.com/glendmaatita/status/1766417693677433131", // 5:56 PM · Mar 9, 2024
       "https://twitter.com/gadingnstn/status/1766487332528504988", // 10:32 PM · Mar 9, 2024
       "https://twitter.com/itsfaqih_/status/1766483265224151166", // 10:16 PM · Mar 9, 2024
+    ],
+  },
+  {
+    title: "Performance 300ms",
+    slug: "300ms",
+    description: "Sepenting itukah performance 300ms?",
+    tweets: [
+      "https://twitter.com/lwastuargo/status/1711903789660451030", // 7:37 AM · Oct 11, 2023
+      "https://twitter.com/gadingnstn/status/1711912952444620975", // 8:13 AM · Oct 11, 2023
+      "https://twitter.com/zakkafm/status/1711917377858969683", // 8:31 AM · Oct 11, 2023
+      "https://twitter.com/ibamarief/status/1711918572061249625", // 8:36 AM · Oct 11, 2023
+      "https://twitter.com/gadingnstn/status/1711928763892789718", // 9:16 AM · Oct 11, 2023
+      "https://twitter.com/lynxluna/status/1711935480869392650", // 9:43 AM · Oct 11, 2023
+      "https://twitter.com/gadingnstn/status/1711949064215187786", // 10:37 AM · Oct 11, 2023
+      "https://twitter.com/gadingnstn/status/1712055544675180921", // 5:40 PM · Oct 11, 2023
     ],
   },
   {
@@ -46,6 +64,9 @@ export const debates: Debates = [
     title: "ORM vs Non-ORM",
     slug: "orm-vs-non-orm",
     description: "Pertarungan antara ORM dan Non-ORM.",
-    tweets: ["https://twitter.com/papanberjalan/status/1758656922834907523"],
+    tweets: [
+      "https://twitter.com/qepo_s/status/1702571153657852077", // 1:32 PM · Sep 15, 2023
+      "https://twitter.com/papanberjalan/status/1758656922834907523",
+    ],
   },
 ];
