9d8283b20514553914060ee06d1541b5ad8db265
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-11 11:43:14 +0800

    feat: remove redis password

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -6,7 +6,7 @@ const redisDev = new Redis();
 const redisProd = new Redis({
   host: ENV.REDIS_HOST,
   port: ENV.REDIS_PORT,
-  password: ENV.REDIS_PASSWORD,
+  // password: ENV.REDIS_PASSWORD,
 });
 
 export const redis = ENV.NODE_ENV === "production" ? redisProd : redisDev;
