4fa9acb4587059442ac93e59cf644dc8e0879883
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-11 14:46:09 +0800

    fix: cascade props

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -24,11 +24,11 @@ export const MinusIcon = ({ className }: { className: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
     className={clsx(className)}
   >
-    <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
   </svg>
 );
 
