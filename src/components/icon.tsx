e202214314bfe0009dda0578abbeccdf854bdcde
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-11 16:43:44 +0800

    fix: stroke-width attribute in ArrowRightIcon component

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -56,7 +56,7 @@ export const ArrowRightIcon = ({ className }: { className: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
     className={clsx(className)}
   >
