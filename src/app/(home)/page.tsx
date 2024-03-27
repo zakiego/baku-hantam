a10c6b6fd7bb106560e37d78a29136926565262b
Romeo Noveanre <noveanrer@gmail.com>
2024-03-27 19:21:19 +0700

    fix/improve: change some ui detail, optimizing query and fix leaderboard issue.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,15 +12,15 @@ export default async function Page() {
     <div>
       <Header />
       <Container>
-        {data.map(async (item, id) => {
+        {data.map(async (item) => {
           return (
             <Link key={item.topic.id} href={`/topic/${item.topic.id}`}>
-              <div className="p-4 my-4 bg-white rounded-md shadow-md cursor-pointer">
+              <div className="px-4 py-8 my-4 bg-white border-b border-b-slate-200 cursor-pointer">
                 <div className="flex -space-x-2 overflow-hidden">
                   {item.users.map((avatar, id) => (
                     <img
                       key={`${id}-${avatar}`}
-                      className="inline-block h-8 w-8xxw rounded-full ring-2 ring-white"
+                      className="inline-block h-7 w-7 rounded-full ring-2 ring-white"
                       src={avatar as string}
                       alt=""
                     />
