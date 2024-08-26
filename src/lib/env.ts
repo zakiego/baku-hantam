05c8ffc563093dd23371a44b9443d108d839592b
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-08-27 02:34:23 +0800

    feat: Add API_URL to .env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-import dotnev from "dotenv";
 
-dotnev.config();
-
-const schema = z.object({
-  NODE_ENV: z.enum(["development", "production"]),
-  DATABASE_URL: z.string().min(5),
+export const ENV = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    DATABASE_URL: z.string().min(5),
+    API_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.OPEN_AI_API_KEY,
+    API_URL: process.env.API_URL,
+  },
 });
-
-export const ENV = schema.parse(process.env);
