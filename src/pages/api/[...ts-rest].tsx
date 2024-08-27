aceba4bc082b8cc0c925899108692aae138837e0
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-08-27 15:47:43 +0800

    feat: remove ts-rest server

--- src/pages/api/[...ts-rest].tsx
+++
@@ -1,5 +0,0 @@
-import { restContract } from "@/lib/api/contract";
-import { restRouter } from "@/lib/api/router";
-import { createNextRouter } from "@ts-rest/next";
-
-export default createNextRouter(restContract, restRouter);
