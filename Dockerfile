e8c8fdac7d37779f611a1f51cbda23e9e39e9d3f
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-08-27 17:44:25 +0800

    feat: Set NEXT_PUBLIC_API_URL environment variable in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --prod --frozen-l
 FROM base AS build
 RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
 RUN pnpm run build
+ENV NEXT_PUBLIC_API_URL=https://api.bakuhantam.dev
 
 FROM base
 COPY --from=prod-deps /app/node_modules /app/node_modules
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,14 +4,12 @@ import { z } from "zod";
 export const ENV = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]),
-    DATABASE_URL: z.string().min(5),
   },
   client: {
     NEXT_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 });
