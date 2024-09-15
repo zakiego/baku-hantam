6a688f6012a8f919a7eb8289841b855178dc54b7
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-09-15 16:19:51 +0800

    feat: Add REVALIDATE_TIME on home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,11 @@ import PageClientHome from "@/app/(home)/page-client";
 import { Container } from "@/components/container";
 import { Header } from "@/components/header";
 import { restClient } from "@/lib/api/client";
+import { REVALIDATE_TIME } from "@/lib/const";
 import Link from "next/link";
 
 export const dynamic = "force-static";
+export const revalidate = REVALIDATE_TIME;
 
 export default async function Page() {
   const resp = await restClient.getAllTopics();
